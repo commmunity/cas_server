7842bb7a85ad9ab79bf097705335c1b39e67a895
Renaud (Nel) Morvan <renaud.morvan@feedback20.com>
2009-02-11 02:34:28 +0100

    fix: lighttpd rack middleware now parse query string

--- lib/cas_server/rack/lighttpd.rb
+++ lib/cas_server/rack/lighttpd.rb
@@ -10,7 +10,9 @@ module CasServer
       
       def call(env)
         if env['SERVER_SOFTWARE'] =~ LIGHTY
-          env['PATH_INFO'] ||= env['REQUEST_URI']
+          request = (env['REQUEST_URI']||'').split('?',2)
+          env['PATH_INFO'] = request[0] || ''
+          env['QUERY_STRING'] = request[1] || ''
           env['SCRIPT_NAME'] = ''
         end
         @app.call(env)
